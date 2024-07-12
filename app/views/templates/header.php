7e5174cd0ba5cb491fa5caf78f6b8185b7e5ef8c
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-07-12 11:19:33 +0000

    updated login page layout

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['auth'])) {
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="icon" href="/favicon.png">
-        <title>COSC 4806</title>
+        <title>Reminder App</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width">
         <meta name="apple-mobile-web-app-capable" content="yes">
@@ -17,7 +17,7 @@ if (!isset($_SESSION['auth'])) {
     <body>
       <nav class="navbar navbar-expand-lg bg-body-tertiary" border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/home">COSC 4806</a>
+    <a class="navbar-brand" href="/home">Reminder App</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
