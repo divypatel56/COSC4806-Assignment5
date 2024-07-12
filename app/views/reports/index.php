ef90097c88da7ecb0d74caf3f9e28526c5341219
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-07-12 10:23:39 +0000

    Added reports index page

---
+++ app/views/reports/index.php
@@ -0,0 +1,12 @@
+<?php require_once 'app/views/templates/header.php' ?>
+
+<div class="container mt-5">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="display-4 text-center">Admin Reports</h1>
+                <hr>
+            </div>
+        </div>
+    </div>
+
